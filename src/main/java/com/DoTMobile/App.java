9f61e0433a529af06a9f48ddf9e336b15c7d9825
kimi agarwal <kimiagarwal@gmail.com>
2017-04-23 18:45:04 +0530

    add two numbers

--- src/main/java/com/DoTMobile/App.java
+++ src/main/java/com/DoTMobile/App.java
@@ -8,7 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+    	int a= 10, b =12, c;
+    	c = a+b;
+    	System.out.println("Sum of two number::" +c);
         System.out.println( "Hello World!" );
         System.out.println("How are you?");
+        
     }
 }
